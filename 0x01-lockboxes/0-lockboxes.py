4dbbcc8640357b7747bc2c16ddb9b410e9691a6c
laila22haz <hazmirlaila316@gmail.com>
2024-01-12 01:50:26 +0100

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,14 +2,20 @@
 
 def canUnlockAll(boxes):
     """
-        boxes:  list of lists
+    Check if it's possible to unlock all the boxes based on a set of keys.
+
+    Parameters:
+    - boxes (List[List[int]]): A list of lists.
+
+    Returns:
+    - bool: True if all boxes can be unlocked, False otherwise.
     """
     array_keys = []
     len_boxes = len(boxes)
     count = 0
 
     for key in boxes[0]:
-        if key not in array_keys and key < len_boxes and key > 0:
+        if key not in array_keys and 0 < key < len_boxes:
             array_keys.append(key)
             count += 1
 
@@ -17,12 +23,9 @@ def canUnlockAll(boxes):
     while index < len(array_keys):
         setkey = array_keys[index]
         for key in boxes[setkey]:
-            if key not in array_keys and key < len_boxes and key > 0:
+            if key not in array_keys and 0 < key < len_boxes:
                 array_keys.append(key)
                 count += 1
         index += 1
 
-    if (count == len_boxes-1):
-        return True
-    else:
-        return False
+    return count == len_boxes - 1
