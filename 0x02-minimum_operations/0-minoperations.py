c9c44cf8916f539a89ec54fb55ee64ba5653898a
laila22haz <hazmirlaila316@gmail.com>
2024-01-19 13:10:29 +0100

    Yes I GET IT

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,15 +3,39 @@
 Minimum operations
 """
 
-
 def minOperations(n):
-    """
-
-    :param n:
-    :return:
-    """
+    """Copy/Paste"""
     if n <= 1:
         return 0
-    for op in range(2, n+1):
-        if n % op == 0:
-            return minOperations(int(n/op)) + op
+    
+    operation_needed = 0
+    clipbord = 0
+    current_lenght = 1
+
+    while current_lenght < n:
+        # If n is divisible by current length, we can copy all
+        if n % current_lenght == 0:
+            # This is the only time we can copy
+            clipbord = current_lenght
+            operation_needed += 1
+
+        current_lenght += clipbord
+        operation_needed += 1
+    
+    return operation_needed
+
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 0
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 10
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
