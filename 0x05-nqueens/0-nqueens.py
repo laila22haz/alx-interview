97731f58ec42bd6433d01e6b3a2b0d35c7f49451
laila22haz <hazmirlaila316@gmail.com>
2024-02-11 22:54:52 +0100

    *-*

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,104 +1,59 @@
 #!/usr/bin/python3
-""" N queen task """
+"""doc doc doc"""
+import sys
 
-from sys import argv, exit
+
+def solve_queens_problem(board_size):
+    """doc doc doc"""
+
+    def is_valid_position(pos, occupied_pos):
+        """doc doc doc"""
+        for i in range(len(occupied_pos)):
+            if (
+                occupied_pos[i] == pos or
+                occupied_pos[i] - i == pos - len(occupied_pos) or
+                occupied_pos[i] + i == pos + len(occupied_pos)
+            ):
+                return False
+        return True
+
+    def place_queens(board_size, index, occupied_pos, solutions):
+        """doc doc doc"""
+        if index == board_size:
+            solutions.append(occupied_pos[:])
+            return
+
+        for i in range(board_size):
+            if is_valid_position(i, occupied_pos):
+                occupied_pos.append(i)
+                place_queens(board_size, index + 1, occupied_pos, solutions)
+                occupied_pos.pop()
+
+    solutions = []
+    place_queens(board_size, 0, [], solutions)
+    return solutions
 
 
-class Board:
-    """ board class """
+def main():
+    """doc doc doc"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-    def __init__(self, n):
-        """
-        board constructor
-        :param n:
-        :type n: integer
-        """
-        self.N = n
-        self.board = [[' ' for _ in range(self.N)] for _ in range(self.N)]
+    try:
+        board_size = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
 
-    def get_solution(self):
-        """Return the matrix representation of a solved chessboard.
-        """
-        solutions = []
-        for r in range(self.N):
-            for c in range(self.N):
-                if self.board[r][c] == "Q":
-                    solutions.append([r, c])
-                    break
-        return solutions
+    if board_size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-    def clone(self):
-        new = Board(self.N)
-        for i in range(self.N):
-            for j in range(self.N):
-                new.board[i][j] = self.board[i][j]
-        return new
-
-    def Xout(self, row, col):
-        """
-        all spots where non-attacking queens can no
-        longer
-        """
-        for c in range(col + 1, self.N):
-            self.board[row][c] = "x"
-        for c in range(col - 1, -1, -1):
-            self.board[row][c] = "x"
-        for r in range(row + 1, self.N):
-            self.board[r][col] = "x"
-        for r in range(row - 1, -1, -1):
-            self.board[r][col] = "x"
-        c = col + 1
-        for r in range(row + 1, self.N):
-            if c >= self.N:
-                break
-            self.board[r][c] = "x"
-            c += 1
-        c = col - 1
-        for r in range(row - 1, -1, -1):
-            if c < 0:
-                break
-            self.board[r][c] = "x"
-            c -= 1
-        c = col + 1
-        for r in range(row - 1, -1, -1):
-            if c >= self.N:
-                break
-            self.board[r][c] = "x"
-            c += 1
-        c = col - 1
-        for r in range(row + 1, self.N):
-            if c < 0:
-                break
-            self.board[r][c] = "x"
-            c -= 1
-
-    def back_track(self, r, queens, solutions):
-        """ return the solution using backtrack technique.
-        """
-        if queens == self.N:
-            solutions.append(self.get_solution())
-            return solutions
-        for c in range(self.N):
-            if self.board[r][c] == " ":
-                cpy = self.clone()
-                cpy.board[r][c] = "Q"
-                cpy.Xout(r, c)
-                solutions = cpy.back_track(r + 1, queens + 1, solutions)
-                # return cpy.back_track(r + 1, queens + 1, solutions)
-        return solutions
+    solutions = solve_queens_problem(board_size)
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(len(solution))])
 
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if not argv[1].isdigit():
-        print("N must be a number")
-        exit(1)
-    N = int(argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-    board = Board(N)
-    for solution in board.back_track(0, 0, []):
-        print(solution)
+    main()
