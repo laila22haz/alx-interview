125395460c845095d38e2e77dddc90d982bf9b89
laila22haz <hazmirlaila316@gmail.com>
2024-03-15 02:20:48 +0000

    Prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
-""" Prime Game """
+""" Module for solving prime game problem using the eratosthenes algorithm.
+"""
 
 
 def isWinner(x, nums):
-    """ Prime Game """
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def calculate_winner(n):
-        """ Prime Game """
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        if len(primes) % 2 == 0:
-            return "Ben"
-        else:
-            return "Maria"
-
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        winner = calculate_winner(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """winner in the prime game.
+    """
+    if not nums or x < 1:
         return None
+    numMax = max(nums)
+
+    list_filter = [True for _ in range(max(numMax + 1, 2))]
+    for i in range(2, int(pow(numMax, 0.5)) + 1):
+        if not list_filter[i]:
+            continue
+        for j in range(i * i, numMax + 1, i):
+            list_filter[j] = False
+    list_filter[0] = list_filter[1] = False
+    y = 0
+    for i in range(len(list_filter)):
+        if list_filter[i]:
+            y += 1
+        list_filter[i] = y
+    firstPlayer = 0
+    for x in nums:
+        firstPlayer += list_filter[x] % 2 == 1
+    if firstPlayer * 2 == len(nums):
+        return None
+    if firstPlayer * 2 > len(nums):
+        return "Maria"
+    return "Ben"
